1854f5564a8938202d75a436aad872d5ee1ae7d2
Ashish Yadav <yashish9990@gmail.com>
2026-06-08 00:19:59 +0530

    loading and Rendering the meal details via dynamin routing.

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,34 @@
-export default function MealsDetailsPage() {
+import Link from "next/link";
+import classes from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
+
+export default async function MealsDetailsPage({ params }) {
+  const meal = await getMeal(params.mealSlug);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+  
+  console.log('meal.........:', meal);
+
   return (
     <>
-      <header></header>
-      <main></main>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            By <Link href={`mailto:${meal.creatorEmail}`}>{meal.creator}</Link>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
     </>
   );
 }
